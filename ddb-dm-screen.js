96b46ce8a20ed0686fcbc29eda186038fe5162d2
mivalsten <mivalsten@gmail.com>
2019-07-11 12:51:24 +0200

    Update ddb-dm-screen.js

--- ddb-dm-screen.js
+++ ddb-dm-screen.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         ddb-dm-screen
 // @namespace    http://tampermonkey.net/
-// @version      0.9
-// @description  try to take over the world!
+// @version      1.0
+// @description  Poor man's DM screen for DDB campaigns
 // @author       You
 // @match        https://www.dndbeyond.com/campaigns/*
 // @grant        none
@@ -43,6 +43,7 @@ class Stat {
     'use strict';
 
     // Your code here...
+    $('#site').after('<div id="iframeDiv" style="display: none;"></iframe><div>');
     var characterJSON = [];
     var characterData = [];
     //console.log(characterJSON);
@@ -60,13 +61,24 @@ class Stat {
                         var character = new Object();
                         // assign character data
                         character.name = x.name;
+                        $('#iframeDiv').append('<iframe id="hiddenFrame' + character.name.replace(/\s/g, '') + '" src="'+ charLink.attr('href') +'"></iframe>');
+                        $('#hiddenFrame' + character.name.replace(/\s/g, '')).load(function(){
+                            setTimeout(function () {
+                                console.log('################### ' + character.name + ' ########################');
+                                console.log($('#hiddenFrame' + character.name.replace(/\s/g, '')).contents());
+                                var acElem = $('#hiddenFrame' + character.name.replace(/\s/g, '')).contents().find(".ct-combat-mobile__extra--ac");
+                                console.log(acElem);
+                                var ac = acElem.find(".ct-combat-mobile__extra-value").text();
+                                $('#character-details-'+character.name.replace(/\s/g, '')+' > tbody:last-child').append('<tr><td>AC</td><td>'+ ac + '</td><td></td><td></td></tr>');
+                            }, 5000);
+                            });
                         character.stats = new Object();
-                        character.stats.str = new Stat(x.stats[0].value);
-                        character.stats.dex = new Stat(x.stats[1].value);
-                        character.stats.con = new Stat(x.stats[2].value);
-                        character.stats.int = new Stat(x.stats[3].value);
-                        character.stats.wis = new Stat(x.stats[4].value);
-                        character.stats.cha = new Stat(x.stats[5].value);
+                        if (x.overrideStats[0].value == null) {character.stats.str = new Stat(x.stats[0].value + x.bonusStats[0].value);} else {character.stats.str = new Stat(x.overrideStats[0].value);}
+                        if (x.overrideStats[1].value == null) {character.stats.dex = new Stat(x.stats[1].value + x.bonusStats[1].value);} else {character.stats.dex = new Stat(x.overrideStats[1].value);}
+                        if (x.overrideStats[2].value == null) {character.stats.con = new Stat(x.stats[2].value + x.bonusStats[2].value);} else {character.stats.con = new Stat(x.overrideStats[2].value);}
+                        if (x.overrideStats[3].value == null) {character.stats.int = new Stat(x.stats[3].value + x.bonusStats[3].value);} else {character.stats.int = new Stat(x.overrideStats[3].value);}
+                        if (x.overrideStats[4].value == null) {character.stats.wis = new Stat(x.stats[4].value + x.bonusStats[4].value);} else {character.stats.wis = new Stat(x.overrideStats[4].value);}
+                        if (x.overrideStats[5].value == null) {character.stats.cha = new Stat(x.stats[5].value + x.bonusStats[5].value);} else {character.stats.cha = new Stat(x.overrideStats[5].value);}
                         // level
                         character.level = 0;
                         for (var j=0; j < x.classes.length; j++) {
@@ -109,8 +121,19 @@ class Stat {
                                     case 'charisma-saving-throws': character.stats.cha.saveProficiency = true; break;
                                 };
                             };
+                            if (y.type == "set") {
+                                switch(y.subType) {
+                                    case 'strength-score': character.stats.str.value = y.value; break;
+                                    case 'dexterity-score': character.stats.dex.value = y.value; break;
+                                    case 'constitution-score': character.stats.con.value = y.value; break;
+                                    case 'intelligence-score': character.stats.int.value = y.value; break;
+                                    case 'wisdom-score': character.stats.wis.value = y.value; break;
+                                    case 'charisma-score': character.stats.cha.value = y.value; break;
+                                }
+                            };
                         };
-                        character.maxHP = x.baseHitPoints + (character.level * parseInt(character.stats.con.bonus())) - x.removedHitPoints;
+                        if (x.overrideHitPoints == null) {character.maxHP = x.baseHitPoints + (character.level * parseInt(character.stats.con.bonus()));}
+                        else {character.maxHP = x.overrideHitPoints;}
                         character.currentHP = character.maxHP + x.temporaryHitPoints - x.removedHitPoints;
 
                         //characterData.push(character);
