fbec57f18cdab7d40554015c133b3a155c14b5f5
mivalsten <mivalsten@gmail.com>
2019-07-18 18:24:00 +0200

    Update ddb-dm-screen.user.js

--- ddb-dm-screen.user.js
+++ ddb-dm-screen.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ddb-dm-screen-2
 // @namespace    https://github.com/mivalsten/ddb-dm-screen
-// @version      2.0.1
+// @version      2.0.2
 // @description  Poor man's DM screen for DDB campaigns
 // @author       You
 // @match        https://www.dndbeyond.com/campaigns/*
