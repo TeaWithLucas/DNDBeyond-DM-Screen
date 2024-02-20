54ae34b070dd9cffacf02d80f824f7991948bac6
FaithLilley <faithelilley@gmail.com>
2024-02-20 00:11:13 +0000

    css

--- ddb-live-campaign.user.js
+++ ddb-live-campaign.user.js
@@ -173,6 +173,7 @@ function defineHTMLsnippets() {
     let htmlSnippetData = `
             <div class="ddb-lc-character-stats">
                 <div class="ddb-lc-character-stats-armorclass">
+                    <div class="ddb-lc-background-svg">` + svgImageData.armorClass + `</div>
                 </div>
                 <div class="ddb-lc-character-stats-hitpoints">
                 </div>
